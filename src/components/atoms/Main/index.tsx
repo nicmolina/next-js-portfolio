9df6ae30ee7a379ffd8a7a1c49269ee202c2feb8
nicmolina <nicolasmarcondes99@gmail.com>
2023-04-02 17:57:44 -0300

    initial setup from nextjs boilerplate

---
+++ src/components/atoms/Main/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const Main: FC = () => {
+  return (
+    <main>
+      <h1>React Avançado</h1>
+    </main>
+  )
+}
+
+export default Main
---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Nextjs Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple start project to work with NextJs and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
